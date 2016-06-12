878933650a4f92055cdda26a0d49c9107f2e165f
Gwen Dipert <gfdipert@gmail.com>
2016-06-12 19:50:38 -0400

    Model for list Items and associated migrations

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+#To give our Item class a save method, and to make it into a real Django model, we make it inherit from the Model class:
+class Item(models.Model):
+	text = models.TextField(default='')

7ccf0898d12d85a36aaed93d1f26ea9006c42db3
Gwen Dipert <gfdipert@gmail.com>
2016-06-19 18:05:45 -0400

    Added test for new user Francis

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -32,7 +33,11 @@ class NewVisitorTest(LiveServerTestCase):
 		)
 
 		inputbox.send_keys('Buy peacock feathers')
+
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+		#assert Regex is a helper function from unittest that checks whether a string matches a regular expression
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -41,6 +46,31 @@ class NewVisitorTest(LiveServerTestCase):
 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+		#new user Francis comes along to the site
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#there is no sign of Edith's lists
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		#Francis starts a new list by entering a new item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		#Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		#No trace of Edith's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 		
 		self.fail('Finish the test!')
 
